1d4833257d8ece50609433b2cc9e50b48278f075
Vanessa  Carvalho <80365265+DevVane@users.noreply.github.com>
2021-05-15 16:59:58 -0300

    Desafio 2: cubos checkout

--- rotas.js
+++ rotas.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const { getProdutos} = require('./controladores/produtos');
+const { getProdutos, 
+        detalharCarrinho, 
+        addAoCarrinho, 
+        editarQtdNoCarrinho, 
+        excluirProdutoDoCarrinho,
+        deleteLimparCarrinho,
+        finalizarCompra} = require('./controladores/produtos');
 
 const rotas = express();
 
 rotas.get('/produtos', getProdutos);
-rotas.get('/carrinho');
-
+rotas.get('/carrinho', detalharCarrinho);
+rotas.post('/carrinho/produtos', addAoCarrinho);
+rotas.patch('/carrinho/produtos/:idProduto', editarQtdNoCarrinho);
+rotas.delete('/carrinho/produtos/:idProduto', excluirProdutoDoCarrinho);
+rotas.delete('/carrinho', deleteLimparCarrinho);
+rotas.post('/finalizar-compra', finalizarCompra);
 
 
 module.exports = rotas;
